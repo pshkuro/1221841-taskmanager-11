cb27b37f87b7a0436c046960ece6b84b3745d871
Полина <polina110297@yandex.ru>
2020-04-03 13:36:52 +0300

    Импортируем компоненты в точку входа

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-
+import {createSiteMenuTemplate} from "./components/site-menu";
+import {createFilterTemplate} from "./components/filter";
+import {createBoardTemplate} from "./components/boards";
+import {createSortingTemplate} from "./components/sorting";
+import {createTaskEditTemplate} from "./components/task-edit";
+import {createTaskTemplate} from "./components/task";
+import {createLoadMoreButtonTemplate} from "./components/load-more-button";
 
 const TASK_COUNT = 3;
 const siteMainElement = document.querySelector(`.main`);
@@ -11,7 +17,6 @@ const render = (container, template, place) => {
 };
 
 
-// Отрисовываем Меню, Фильтры, Сортировку
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 render(siteMainElement, createFilterTemplate(), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
@@ -19,15 +24,13 @@ render(siteMainElement, createBoardTemplate(), `beforeend`);
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 const boardElement = siteMainElement.querySelector(`.board`);
 
-// Отрис Карточку создания/ред задачи
+render(boardElement, createSortingTemplate(), `afterbegin`);
 render(taskListElement, createTaskEditTemplate(), `beforeend`);
 
-// Отрис Карточки задачи
 for (let i = 0; i < TASK_COUNT; i++) {
   render(taskListElement, createTaskTemplate(), `beforeend`);
 }
 
-// Отрис кнопки 'Load more'
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
 
 
