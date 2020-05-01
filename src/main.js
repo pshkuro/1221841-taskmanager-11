3a1d10f4c47a723ba1ef5f82bbf6c645a69fa741
Полина <polina110297@yandex.ru>
2020-05-01 16:26:19 +0300

    Show Tasks Statistics

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import SiteMenuCopmonent, {MenuItem} from "./components/site-menu";
 import BoardCopmonent from "./components/boards";
 import BoardController from "./controllers/board";
 import FiltersController from "./controllers/filters";
+import StatisticsComponent from "./components/statistics";
 import {generateTasks} from "./mock/task";
 import {renderPosition, render} from "./utils/render";
 import TasksModel from "./models/tasks";
+
 
 const TASK_COUNT = 22;
 const siteMainElement = document.querySelector(`.main`);
@@ -30,11 +32,32 @@ const boardController = new BoardController(boardComponent, tasksModel, () => {
 });
 boardController.render();
 
+const dateTo = new Date();
+const dateFrom = (() => {
+  const d = new Date(dateTo);
+  d.setDate(d.getDate() - 7);
+  return d;
+})();
+const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+render(siteMainElement, statisticsComponent, renderPosition.BEFOREEND);
+statisticsComponent.hide();
+
 siteMenuComponent.setOnChange((menuItem) => {
   switch (menuItem) {
     case MenuItem.NEW_TASK:
       siteMenuComponent.setActiveItem(MenuItem.TASKS);
+      statisticsComponent.hide();
+      boardController.show();
       boardController.createTask();
+      break;
+    case MenuItem.STATISTICS:
+      filterController.rerender();
+      boardController.hide();
+      statisticsComponent.show();
+      break;
+    case MenuItem.TASKS:
+      statisticsComponent.hide();
+      boardController.show();
       break;
   }
 });
